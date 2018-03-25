b99640827344d9750549bebb4fe0376441f3e929
Petr Patek <petr.patek@plus4u.net>
2018-03-25 12:50:18 +0200

    stack implemented

--- stack.rb
+++ stack.rb
@@ -24,34 +24,45 @@ end
 class SinglyListStack
   def initialize
     # TODO
+    @stack = SinglyLinkedList.new
   end
 
   def push(x)
     # returns self
     # TODO
+    @stack << x
+    self
   end
 
   def pop
     # returns the removed value
     # raises StackEmptyException if stack is empty
     # TODO
+    raise StackEmptyException.new(@stack) if @stack.length == 0
+    @stack.delete_at(@stack.length - 1)
   end
 end
 
 class BooleanArrayStack
   def initialize(size)
     # TODO
+    @stack = BooleanArray.new(size)
   end
 
   def push(x)
     # returns self
     # raises StackFullException if stack is already full
     # TODO
+    raise StackFullException.new(@stack) if @stack.length == @stack.used_length
+    @stack << x
+    self
   end
 
   def pop
     # returns the removed value
     # raises StackEmptyException if stack is empty
     # TODO
+    raise StackEmptyException.new(@stack) if @stack.used_length == 0
+    @stack.delete_at(@stack.used_length - 1)
   end
 end
