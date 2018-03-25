1accdee97c02851a241937a274163be6b038176f
Petr Patek <petr.patek@plus4u.net>
2018-03-25 13:32:03 +0200

    BooleanArray queue implemented queue and dequeue

--- queue.rb
+++ queue.rb
@@ -45,18 +45,30 @@ end
 class BooleanArrayQueue
   def initialize(size)
     # TODO
+    @queue = BooleanArray(size)
+    @start = 0
+    @count = 0
   end
 
   def queue(x)
     # returns self
     # raises QueueFullException if queue is already full
     # TODO
+    raise QueueFullException.new(self) if @count + 1 > @queue.length
+    @queue[(@start + @count) % @queue.length] = x
+    @count += 1
+    return self
   end
 
   def dequeue
     # returns the removed value
     # raises QueueEmptyException if queue is empty
     # TODO
+    raise QueueEmptyException.new(self) if @count < 1
+    item = @queue[@start]
+    @start = (@start + 1) % @queue.length
+    @count -= 1
+    return item
   end
 end
 
